27359dc270144053c3675507920c5322aa07bc4a
Fathur Danu <fathurdanu@gmail.com>
2024-07-09 14:43:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+require('dotenv').config()
+const fs = require('fs');
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/points", (req, res) => {
+    try {
+        fs.readFile('./data/points.json', 'utf8', function (err, data) {
+            const result = JSON.parse(data)
+            const limit = Number(req?.query?.limit);
+            const skip = Number(req?.query?.skip) || 0;
+            const finalResult = result.slice(skip, limit ? skip + limit : result?.length)
+            const meta = {
+                totalData: result?.length,
+                data: finalResult
+            }
+            res.status(200).json(meta);
+        })
+    } catch (e) {
+        res.status(400).json(e)
+    }
+});
+
+app.get("/points/:id", (req, res) => {
+    try {
+        fs.readFile('./data/points.json', 'utf8', function (err, data) {
+            let points = JSON.parse(data);
+            const id = Number(req.params.id);
+            const point = points.find(point => Number(point.id) === id);
+            res.status(200).json(point);
+        })
+    } catch (e) {
+        res.status(400).json(e)
+    }
+});
+
+app.post("/points", (req, res) => {
+    try {
+        fs.readFile('./data/points.json', 'utf8', function (err, data) {
+            const points = JSON.parse(data)
+            const lastPoint = points?.at(-1)
+            let point = req?.body;
+            const newId = Number(lastPoint?.id) + 1;
+            point.id = newId
+            points.push(point)
+            let strJson = JSON.stringify(points);
+            fs.writeFileSync('./data/points.json', strJson, 'utf8');
+            res.status(201).json({
+                id: newId
+            });
+        })
+    } catch (e) {
+        res.status(400).json(e)
+    }
+})
+
+app.put("/points/:id", (req, res) => {
+    try {
+        if (!req?.params?.id) throw 0;
+        if (!req?.body) throw 0;
+        fs.readFile('./data/points.json', 'utf8', function (err, data) {
+            let points = JSON.parse(data);
+            const id = Number(req.params.id);
+            const pointIndex = points.findIndex(point => Number(point.id) === id);
+
+            let newPoint = req?.body;
+            newPoint.id = Number(id);
+
+            points[pointIndex] = newPoint;
+            let strJson = JSON.stringify(points);
+            fs.writeFileSync('./data/points.json', strJson, 'utf8');
+            res.status(201).send("success");
+        })
+    } catch (e) {
+        res.status(400).json(e)
+    }
+})
+
+app.delete("/points/:id", (req, res) => {
+    try {
+        if (!req?.params?.id) throw 0;
+        fs.readFile('./data/points.json', 'utf8', function (err, data) {
+            let points = JSON.parse(data);
+            const id = Number(req.params.id);
+            points = points.filter(point => Number(point.id) !== id);
+            let strJson = JSON.stringify(points);
+            fs.writeFileSync('./data/points.json', strJson, 'utf8');
+            res.status(201).send("success");
+        })
+    } catch (e) {
+        res.status(400).json(e)
+    }
+})
+
+
+app.listen(port, () => {
+    console.log(`App is listening on ${port}`);
+})
